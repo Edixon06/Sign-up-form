fdd18ec2a867f340593df1ce700420784e0e7a1a
Edixon Serrano <edixon.serrano06@gmail.com>
2021-03-29 20:15:46 -0400

    refactory: funcionalidad para desplegar modal

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
-let loginButton = document.getElementById('login-button'),
-    closeButton = document.getElementById('close-button');
+let body = document.getElementById('body'),
+    loginButton = document.getElementById('login-button'),
+    closeButton = document.getElementById('close-button'),
     modal = document.getElementById('modal');
 
-loginButton.addEventListener('click', () => {
-    modal.classList.toggle('modal-active');
-})
-closeButton.addEventListener('click', () => {
-    modal.classList.toggle('modal-active');
+body.addEventListener('click', (e) => {
+    let tgt = e.target;
+    if(tgt === loginButton) {
+        modal.classList.add('modal-active');
+    } else if (tgt === closeButton || tgt === modal) {
+        modal.classList.remove('modal-active');
+    }
 })
